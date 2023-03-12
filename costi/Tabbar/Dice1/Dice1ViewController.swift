6d2b8eef28bb6cbe395d26b01d924c3ba50199e5
Ivan <paramonikhin.ivan@yandex.ru>
2023-03-12 12:30:56 +0300

    One dice game, added roll button

--- costi/Tabbar/Dice1/Dice1ViewController.swift
+++ costi/Tabbar/Dice1/Dice1ViewController.swift
@@ -13,17 +13,12 @@ import Foundation
 class Dice1ViewController: UIViewController {
 
     @IBOutlet weak var diceOne: UIImageView!
-    @IBOutlet weak var diceTwo: UIImageView!
     
-    @IBOutlet weak var summa: UILabel!
+    @IBOutlet weak var rollButton: UIButton!
     
     @IBAction func singleTap(_ sender: UITapGestureRecognizer) {
-        
         var val1 = randomVal()
-        var val2 = randomVal()
-        diceImg(val1, val2)
-        var summ = val1+val2
-        summa.text = "Your score: " + String(summ)
+        diceOne.image = UIImage(named: "\(String(val1))white.png")
     }
     
     override func viewDidLoad() {
@@ -31,44 +26,9 @@ class Dice1ViewController: UIViewController {
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(self.singleTap(_:)))
         singleTap.numberOfTapsRequired = 1
         singleTap.numberOfTouchesRequired = 1
-        view.addGestureRecognizer(singleTap)
-//        diceOne.image = UIImage(named: "1white.png")
+        rollButton.addGestureRecognizer(singleTap)
     }
     
-    func diceImg(_ diceUno: Int, _ diceDos: Int){
-        switch diceUno{
-            case 1:
-                diceOne.image = UIImage(named: "1white.png")
-            case 2:
-                diceOne.image = UIImage(named: "2white.png")
-            case 3:
-                diceOne.image = UIImage(named: "3white.png")
-            case 4:
-                diceOne.image = UIImage(named: "4white.png")
-            case 5:
-                diceOne.image = UIImage(named: "5white.png")
-            case 6:
-                diceOne.image = UIImage(named: "6white.png")
-            default:
-                diceOne.image = UIImage(named: "1white.png")
-        }
-        switch diceDos{
-            case 1:
-                diceTwo.image = UIImage(named: "1black.png")
-            case 2:
-                diceTwo.image = UIImage(named: "2black.png")
-            case 3:
-                diceTwo.image = UIImage(named: "3black.png")
-            case 4:
-                diceTwo.image = UIImage(named: "4black.png")
-            case 5:
-                diceTwo.image = UIImage(named: "5black.png")
-            case 6:
-                diceTwo.image = UIImage(named: "6black.png")
-            default:
-                diceTwo.image = UIImage(named: "1black.png")
-        }
-    }
     func randomVal ()->Int{
         var val:Int = Int.random(in: 1...6)
         return val
--- costi/Tabbar/Dice2/Dice2ViewController.swift
+++ costi/Tabbar/Dice2/Dice2ViewController.swift
@@ -6,24 +6,150 @@
 //
 
 import UIKit
+import Foundation
 
-class Dice2ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+class Dice2ViewController: UIViewController{
+
+    struct PokerDice{
+        var diceVal:Int?
+        var isTap = false
+        init(_ isTap:Bool) {
+            self.isTap = isTap
+        }
     }
     
+    @IBOutlet weak var pokerDiceOneImage: UIImageView!
+    @IBOutlet weak var pokerDiceTwoImage: UIImageView!
+    @IBOutlet weak var pokerDiceThreeImage: UIImageView!
+    @IBOutlet weak var pokerDiceFourImage: UIImageView!
+    @IBOutlet weak var pokerDiceFiveImage: UIImageView!
+    var diceOne = PokerDice(false)
+    var diceTwo = PokerDice(false)
+    var diceThree = PokerDice(false)
+    var diceFour = PokerDice(false)
+    var diceFive = PokerDice(false)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    override func viewDidLoad() {
+        
+//        var diceOne = PokerDice(false)
+        super.viewDidLoad()
+        let screenSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.screenSingleTap(_:)))
+        screenSingleTap.numberOfTapsRequired = 1
+        screenSingleTap.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(screenSingleTap)
+        let pokerDiceOneSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.pokerDiceOneSingleTap(_:)))
+        pokerDiceOneSingleTap.numberOfTapsRequired = 1
+        pokerDiceOneSingleTap.numberOfTouchesRequired = 1
+        pokerDiceOneImage.addGestureRecognizer(pokerDiceOneSingleTap)
+        let pokerDiceTwoSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.pokerDiceTwoSingleTap(_:)))
+        pokerDiceTwoSingleTap.numberOfTapsRequired = 1
+        pokerDiceTwoSingleTap.numberOfTouchesRequired = 1
+        pokerDiceTwoImage.addGestureRecognizer(pokerDiceTwoSingleTap)
+        let pokerDiceThreeSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.pokerDiceThreeSingleTap(_:)))
+        pokerDiceThreeSingleTap.numberOfTapsRequired = 1
+        pokerDiceThreeSingleTap.numberOfTouchesRequired = 1
+        pokerDiceThreeImage.addGestureRecognizer(pokerDiceThreeSingleTap)
+        let pokerDiceFourSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.pokerDiceFourSingleTap(_:)))
+        pokerDiceFourSingleTap.numberOfTapsRequired = 1
+        pokerDiceFourSingleTap.numberOfTouchesRequired = 1
+        pokerDiceFourImage.addGestureRecognizer(pokerDiceFourSingleTap)
+        let pokerDiceFiveSingleTap = UITapGestureRecognizer(target: self, action: #selector(self.pokerDiceFiveSingleTap(_:)))
+        pokerDiceFiveSingleTap.numberOfTapsRequired = 1
+        pokerDiceFiveSingleTap.numberOfTouchesRequired = 1
+        pokerDiceFiveImage.addGestureRecognizer(pokerDiceFiveSingleTap)
     }
-    */
 
+    @IBAction func pokerDiceOneSingleTap(_ sender: Any) {
+        if(!diceOne.isTap)
+        {
+            diceOne.isTap = true
+            pokerDiceOneImage.image = UIImage(named:"\(diceOne.diceVal ?? 1)black.png")
+        }else{
+            diceOne.isTap = false
+            pokerDiceOneImage.image = UIImage(named:"\(diceOne.diceVal ?? 1)white.png")
+        }
+    }
+    
+    @IBAction func pokerDiceTwoSingleTap(_ sender: Any) {
+        if(!diceTwo.isTap)
+        {
+            diceTwo.isTap = true
+            pokerDiceTwoImage.image = UIImage(named:"\(diceTwo.diceVal ?? 1)black.png")
+        }else{
+            diceTwo.isTap = false
+            pokerDiceTwoImage.image = UIImage(named:"\(diceTwo.diceVal ?? 1)white.png")
+        }
+    }
+    
+    @IBAction func pokerDiceThreeSingleTap(_ sender: Any) {
+        if(!diceThree.isTap)
+        {
+            diceThree.isTap = true
+            pokerDiceThreeImage.image = UIImage(named:"\(diceThree.diceVal ?? 1)black.png")
+        }else{
+            diceThree.isTap = false
+            pokerDiceThreeImage.image = UIImage(named:"\(diceThree.diceVal ?? 1)white.png")
+        }
+    }
+    
+    @IBAction func pokerDiceFourSingleTap(_ sender: Any) {
+        if(!diceFour.isTap)
+        {
+            diceFour.isTap = true
+            pokerDiceFourImage.image = UIImage(named:"\(diceFour.diceVal ?? 1)black.png")
+        }else{
+            diceFour.isTap = false
+            pokerDiceFourImage.image = UIImage(named:"\(diceFour.diceVal ?? 1)white.png")
+        }
+    }
+    
+    @IBAction func pokerDiceFiveSingleTap(_ sender: Any) {
+        if(!diceFive.isTap)
+        {
+            diceFive.isTap = true
+            pokerDiceFiveImage.image = UIImage(named:"\(diceFive.diceVal ?? 1)black.png")
+        }else{
+            diceFive.isTap = false
+            pokerDiceFiveImage.image = UIImage(named:"\(diceFive.diceVal ?? 1)white.png")
+        }
+    }
+    
+    @IBAction func screenSingleTap(_ sender: Any) {
+        if(!diceOne.isTap)
+        {
+            diceOne.diceVal = randomVal()
+            pokerDiceOneImage.image = UIImage(named: "\(diceOne.diceVal ?? 1)white.png")        }
+        if(!diceTwo.isTap)
+        {
+            diceTwo.diceVal = randomVal()
+            pokerDiceTwoImage.image = UIImage(named: "\(diceTwo.diceVal ?? 1)white.png")
+        }
+        if(!diceThree.isTap)
+        {
+            diceThree.diceVal = randomVal()
+            pokerDiceThreeImage.image = UIImage(named: "\(diceThree.diceVal ?? 1)white.png")
+        }
+        if(!diceFour.isTap)
+        {
+            diceFour.diceVal = randomVal()
+            pokerDiceFourImage.image = UIImage(named: "\(diceFour.diceVal ?? 1)white.png")
+        }
+        if(!diceFive.isTap)
+        {
+            diceFive.diceVal = randomVal()
+            pokerDiceFiveImage.image = UIImage(named: "\(diceFive.diceVal ?? 1)white.png")
+        }
+    }
+    
+  
+    
+    func randomVal ()->Int{
+        let val:Int = Int.random(in: 1...6)
+        return val
+    }
+    
 }
